2e9aeae64d4aaae342aec3f7bf6cd63d3a564285
smmasudrana <smmasudrana62@gmail.com>
2025-08-06 22:16:15 +0600

    add array looping

---
+++ array-looping.js
@@ -0,0 +1,26 @@
+const fruits = ['apple', 'banana', 'anar', 'watermelon', 'daav']
+for (let i =0; i < fruits.length; i++){
+    console.log(i);
+    console.log(fruits[i]);
+}
+ //with while loop
+let k = 0;
+while (k < fruits.length) {
+console. log(fruits[k]);
+k++;
+}
+
+
+const numbers =[65, 66, 89, 95, 35, 86, 69]
+for(let j = 0; j < numbers.length; j++){
+    console.log(numbers[j]);
+}
+
+
+//while loop
+const prices = [15, 80, 49, 78, 26];
+let n = 0;
+while(n < prices.length){
+console. log (prices[n]);
+n++;
+}
