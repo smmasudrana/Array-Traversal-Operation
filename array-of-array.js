a494462f97e58e8641341b4d1f54bb5655d5fffb
smmasudrana <smmasudrana62@gmail.com>
2025-08-07 12:11:24 +0600

    add two dimensional array

---
+++ array-of-array.js
@@ -0,0 +1,15 @@
+const numbers = [1, 2, 5, 4, 6];
+const tournament_runs = [[], [], []];
+const exam_marks = [
+[98, 87, 45, 12, 63],
+[54, 89, 74, 23, 11],
+[45, 87, 12, 56, 45],
+[78, 65, 32, 87, 54]
+]
+
+console.log(numbers[0]);
+console.log(exam_marks[0]);
+console.log(exam_marks[0][0]);
+
+exam_marks[0][1]=66;
+console.log(exam_marks);
